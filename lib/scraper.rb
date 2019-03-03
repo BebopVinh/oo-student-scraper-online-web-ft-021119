e2af164c3f703a663ff746aea52562bc0f0cff6d
Vinh Do <vinhdo07@gmail.com>
2019-03-03 05:45:28 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,16 +6,26 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
     students = doc.css('div.student-card')
+    array = []
     students.each do |student|
-      name = student.css('h4.student-name').text
-      location = student.css('p.student-location').text
-      profile = student.css('a')
-      binding.pry
+      array << {
+      name: student.css('h4.student-name').text,
+      location: student.css('p.student-location').text,
+      profile_url: student.css('a').first['href'] }
     end
+    array
   end
 
   def self.scrape_profile_page(profile_url)
-
+    doc = Nokogiri::HTML(open(profile_url))
+    social = doc.css('div.social-icon-container a')
+    hash = {
+      twitter: social[0]['href'],
+      linkedin: social[1]['href'],
+      github: social[2]['href'],
+      blog: social[3]['href'],
+      profile_quote: doc.css('div.profile-quote').text,
+      bio: doc.css('div.description-holder p').text}
   end
 
 end
